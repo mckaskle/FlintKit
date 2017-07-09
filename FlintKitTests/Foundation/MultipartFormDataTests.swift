9e018962c18306b87203b50fa333ca1fc282c5ad
Devin McKaskle <devin.mckaskle@gmail.com>
2017-07-09 08:48:17 -0500

    Fixed broken tests.

--- FlintKitTests/Foundation/MultipartFormDataTests.swift
+++ FlintKitTests/Foundation/MultipartFormDataTests.swift
@@ -794,7 +794,7 @@ class MultipartFormDataWriteEncodedDataToDiskTestCase: AFBaseTestCase {
 class MultipartFormDataFailureTestCase: AFBaseTestCase {
     func testThatAppendingFileBodyPartWithInvalidLastPathComponentReturnsError() {
         // Given
-        let fileURL = NSURL(string: "") as! URL
+        let fileURL = NSURL(string: "")! as URL
         let multipartFormData = MultipartFormData()
         multipartFormData.append(fileURL, withName: "empty_data")
 
