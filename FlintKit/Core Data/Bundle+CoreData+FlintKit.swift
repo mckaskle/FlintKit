2e5f8c975a5ee485d8267daffe335c9f23cb7327
Devin McKaskle <devin.mckaskle@gmail.com>
2017-07-10 19:36:22 -0500

    Search the bundle for top level mom files when migrating Core Data.

--- FlintKit/Core Data/Bundle+CoreData+FlintKit.swift
+++ FlintKit/Core Data/Bundle+CoreData+FlintKit.swift
@@ -13,30 +13,26 @@ import Foundation
 extension Bundle {
   
   func path(forManagedObjectModelSourceMetadata metadata: [String: Any]) -> URL? {
-    var momdPaths = paths(forResourcesOfType: "momd", inDirectory: nil) as [NSString]
+    let momdPaths = paths(forResourcesOfType: "momd", inDirectory: nil)
     
-    if
-      let resourceURL = resourceURL,
-      resourceURL.pathExtension == "momd" {
-      // This bundle _is_ a momd directory.
-      momdPaths.append(resourceURL.path as NSString)
+    var modelPaths: [String] = momdPaths.flatMap { (momdPath: String) -> [String] in
+      let directory = (momdPath as NSString).lastPathComponent
+      return paths(forResourcesOfType: "mom", inDirectory: directory)
     }
     
-    for momdPath in momdPaths {
-      let directory = momdPath.lastPathComponent
-      let modelPaths = paths(forResourcesOfType: "mom", inDirectory: directory)
+    // Add any paths of "mom" files in the top level. This can happen if the bundle is the momd directory.
+    modelPaths.append(contentsOf: paths(forResourcesOfType: "mom", inDirectory: nil))
       
-      for modelPath in modelPaths {
-        let path = URL(fileURLWithPath: modelPath)
-        guard
-          let model = NSManagedObjectModel(contentsOf: path),
-          model.isConfiguration(withName: nil, compatibleWithStoreMetadata: metadata) else {
-            continue
-        }
-        
-        
-        return path
+    for modelPath in modelPaths {
+      let path = URL(fileURLWithPath: modelPath)
+      guard
+        let model = NSManagedObjectModel(contentsOf: path),
+        model.isConfiguration(withName: nil, compatibleWithStoreMetadata: metadata) else {
+          continue
       }
+      
+      
+      return path
     }
     
     return nil
