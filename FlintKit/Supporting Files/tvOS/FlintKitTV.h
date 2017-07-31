f049d11d6c93e0c4d026d2d8d5c107b1ab35bb59
Devin McKaskle <devin.mckaskle@gmail.com>
2017-07-31 16:53:12 -0500

    Added new target for tvOS.

---
+++ FlintKit/Supporting Files/tvOS/FlintKitTV.h
@@ -0,0 +1,19 @@
+//
+//  FlintKitTV.h
+//  FlintKitTV
+//
+//  Created by Ranger on 7/31/17.
+//  Copyright © 2017 Devin McKaskle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlintKitTV.
+FOUNDATION_EXPORT double FlintKitTVVersionNumber;
+
+//! Project version string for FlintKitTV.
+FOUNDATION_EXPORT const unsigned char FlintKitTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlintKitTV/PublicHeader.h>
+
+#import <FlintKitTV/TryCatcher.h>
